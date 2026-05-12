c128bbb2ff8d9aa96191f2ffea63b4694653558c
YadneshUmredkar75 <yadneshumredkar75@gmail.com>
2026-05-12 10:42:33 +0530

    Stipend

--- admin/src/components/CertificateTemplates/DataAnalystTemplate.jsx
+++ admin/src/components/CertificateTemplates/DataAnalystTemplate.jsx
@@ -14,6 +14,7 @@ const DataAnalystTemplate = ({
     internshipFrom = "15th Jun 2025",
     internshipTo = "15th August 2025",
     issuedDate = "[Date]",
+    stipend = "[Stipend]",
     
   } = data;
 
@@ -57,6 +58,7 @@ const DataAnalystTemplate = ({
           <p>Position: {position}</p>
           <p>Department: Product & Development</p>
           <p>Internship Duration: {internshipFrom} to {internshipTo}</p>
+          <p>Stipend: {stipend}</p>
         </div>
 
         {/* Responsibilities */}
